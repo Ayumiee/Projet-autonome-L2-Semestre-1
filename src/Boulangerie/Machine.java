1483a284d4d6a4f89528d6c54c3f6ba4a64b15de
Mathilde Darques <mathildedarques.m@gmail.com>
2022-12-20 13:25:09 +0100

    ok

--- src/Boulangerie/Machine.java
+++ src/Boulangerie/Machine.java
@@ -17,16 +17,18 @@ public class Machine {
 		Produit produitDemande = Inventaire.numeroChoisisVersProduit(produit);
 		int prix = produitDemande.getPrix() * nbSouhaite;
 		
-		if ((verifierArgentSuffisant(argent, prix)) && (produitSuffisant(produitDemande, nbSouhaite))) {
-			lancerTransaction(produitDemande, argent, nbSouhaite, prix); //monnaie a utiliser par la suite
-			System.out.println("valide - ok");
-			
-		}else {
-			AnnulerTransaction(argent, distributeur);
-			System.out.println("valide - annule");
-			
+		if (produitSuffisant(produitDemande, nbSouhaite)) {
+			if (verifierArgentSuffisant(argent, prix)) {
+				lancerTransaction(produitDemande, argent, nbSouhaite, prix); //monnaie a utiliser par la suite
+				System.out.println("Transaction acceptée !");
+			}else {
+				AnnulerTransaction(argent, "Argent insuffisant" , distributeur);
+				}
+		} else {
+		AnnulerTransaction(argent, "Nombre souhaité indisponible" , distributeur);
 		}
 	}
+
 		
 	private int lancerTransaction(Produit produitDemande, int argent, int nbSouhaite,int prix) {
 		produitDemande.reduireQuantitee(nbSouhaite);
@@ -35,15 +37,16 @@ public class Machine {
 		return monnaieARendre;
 		}
 
-	private void AnnulerTransaction(int argent, Machine distributeur) {
-		System.out.println("Argent insuffisant");
+	private void AnnulerTransaction(int argent, String nomErreur, Machine distributeur) {
+		System.out.println(nomErreur);
+		System.out.println("Transaction annulée");
 		rendreMonnaie(argent, 0);
 		Main.Menu_client(distributeur);
 	}
 	
-	public boolean produitSuffisant(Produit produit, int quantitee) {
+	public boolean produitSuffisant(Produit produit, int nbSouhaite) {
 		int nombreDispo = produit.getNombre();
-		if ((quantitee < nombreDispo ) && (quantitee >0)) {
+		if ((nbSouhaite <= nombreDispo ) && (nbSouhaite >0)) {
 			return true;
 		}
 		return false;
